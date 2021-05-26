d6ae724a6c7cce46883bed697b5211bbd3285329
MamoshiSE <33344169+MamoshiSE@users.noreply.github.com>
2021-05-27 01:00:30 +0200

    State management for user input

--- App.js
+++ App.js
@@ -1,21 +1,36 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
 
 export default function App() {
+
+  const [enteredGoal, setEnteredGoal] = useState('');
+
+  function goalInputHandler(enteredText) {
+    setEnteredGoal(enteredText);
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.screen}>
+      <View style={styles.inputContainer}>
+      <TextInput placeholder="Course Goal" style={styles.input} onChangeText={goalInputHandler}/>
+      <Button title="Add"/>
+      </View>
+      <View>
+        
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  screen: {
+    padding: 50
   },
+  inputContainer: {
+    flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center'
+  },
+  input: {
+    borderBottomColor: 'black', borderWidth: 1, padding: 10, width: '80%'
+  }
 });
