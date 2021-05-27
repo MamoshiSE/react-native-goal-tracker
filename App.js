c0784fdeb8c7dcd34142de5f387f986fac684bd6
MamoshiSE <33344169+MamoshiSE@users.noreply.github.com>
2021-05-27 17:51:47 +0200

    splitting app.js into components and making flatList view for goals

--- App.js
+++ App.js
@@ -1,31 +1,24 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { useState } from 'react';
-import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
+import GoalItem from './components/GoalItem';
+import GoalInput from './components/GoalInput';
+import { StyleSheet, Text, View, TextInput, Button, ScrollView, FlatList } from 'react-native';
 
+// flatlist is lazyloading use when working with big data
 export default function App() {
-
-  const [enteredGoal, setEnteredGoal] = useState('');
   const [courseGoals, setCourseGoals] = useState([])
 
-  const goalInputHandler = (enteredText) => {
-    setEnteredGoal(enteredText);
-  }
-
-  const addGoalHandler = () => {
+  const addGoalHandler = goalTitle => {
     // take the previous state and adds the new goal to the state
-    setCourseGoals(prevGoals => [...prevGoals, enteredGoal])
+    setCourseGoals(prevGoals => [...prevGoals, { key: Math.random().toString(), value: goalTitle}])
   }
 
   return (
     <View style={styles.screen}>
-      <View style={styles.inputContainer}>
-      <TextInput placeholder="Course Goal" style={styles.input} onChangeText={goalInputHandler}
-      value={enteredGoal}/>
-      <Button title="Add" onPress={addGoalHandler}/>
-      </View>
-      <View>
-      {courseGoals.map((goal, index) => <View style={styles.listItem}><Text key={index}>{goal}</Text></View>)}
-      </View>
+      <GoalInput onAddGoal={addGoalHandler}/>
+      <FlatList data={courseGoals} renderItem={itemData => (
+        <GoalItem title={itemData.item.value}/>
+      )}/>
     </View>
   );
 }
@@ -33,18 +26,5 @@ export default function App() {
 const styles = StyleSheet.create({
   screen: {
     padding: 50
-  },
-  inputContainer: {
-    flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center'
-  },
-  input: {
-    borderBottomColor: 'black', borderWidth: 1, padding: 10, width: '80%'
-  },
-  listItem: {
-    padding: 10,
-    backgroundColor: '#158b05',
-    borderColor: 'black',
-    borderWidth: 1,
-    marginTop: 10
   }
 });
